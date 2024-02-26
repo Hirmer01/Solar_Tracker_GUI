2c4f65501887db4628aab432e0f12a8fe1b1b81d
Hirmer01 <jume.meier@gmail.com>
2024-02-26 16:41:25 +0100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace Solar_Tracker_GUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.leftButton = new System.Windows.Forms.Button();
+            this.noButton = new System.Windows.Forms.Button();
+            this.rightButton = new System.Windows.Forms.Button();
+            this.upButton = new System.Windows.Forms.Button();
+            this.downButton = new System.Windows.Forms.Button();
+            this.stopButton = new System.Windows.Forms.Button();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.resetButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.PortName = "COM5";
+            // 
+            // leftButton
+            // 
+            this.leftButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.leftButton.Location = new System.Drawing.Point(135, 191);
+            this.leftButton.Name = "leftButton";
+            this.leftButton.Size = new System.Drawing.Size(138, 55);
+            this.leftButton.TabIndex = 0;
+            this.leftButton.Text = "left";
+            this.leftButton.UseVisualStyleBackColor = true;
+            this.leftButton.Click += new System.EventHandler(this.leftButton_Click);
+            // 
+            // noButton
+            // 
+            this.noButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.noButton.Location = new System.Drawing.Point(279, 191);
+            this.noButton.Name = "noButton";
+            this.noButton.Size = new System.Drawing.Size(138, 55);
+            this.noButton.TabIndex = 1;
+            this.noButton.Text = "no";
+            this.noButton.UseVisualStyleBackColor = true;
+            this.noButton.Click += new System.EventHandler(this.noButton_Click);
+            // 
+            // rightButton
+            // 
+            this.rightButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rightButton.Location = new System.Drawing.Point(423, 191);
+            this.rightButton.Name = "rightButton";
+            this.rightButton.Size = new System.Drawing.Size(138, 55);
+            this.rightButton.TabIndex = 2;
+            this.rightButton.Text = "right";
+            this.rightButton.UseVisualStyleBackColor = true;
+            this.rightButton.Click += new System.EventHandler(this.rightButton_Click);
+            // 
+            // upButton
+            // 
+            this.upButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.upButton.Location = new System.Drawing.Point(279, 130);
+            this.upButton.Name = "upButton";
+            this.upButton.Size = new System.Drawing.Size(138, 55);
+            this.upButton.TabIndex = 3;
+            this.upButton.Text = "up";
+            this.upButton.UseVisualStyleBackColor = true;
+            this.upButton.Click += new System.EventHandler(this.upButton_Click);
+            // 
+            // downButton
+            // 
+            this.downButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.downButton.Location = new System.Drawing.Point(279, 252);
+            this.downButton.Name = "downButton";
+            this.downButton.Size = new System.Drawing.Size(138, 55);
+            this.downButton.TabIndex = 4;
+            this.downButton.Text = "down";
+            this.downButton.UseVisualStyleBackColor = true;
+            this.downButton.Click += new System.EventHandler(this.downButton_Click);
+            // 
+            // stopButton
+            // 
+            this.stopButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.stopButton.Location = new System.Drawing.Point(279, 366);
+            this.stopButton.Name = "stopButton";
+            this.stopButton.Size = new System.Drawing.Size(138, 55);
+            this.stopButton.TabIndex = 5;
+            this.stopButton.Text = "stop";
+            this.stopButton.UseVisualStyleBackColor = true;
+            this.stopButton.Click += new System.EventHandler(this.stopButton_Click);
+            // 
+            // saveButton
+            // 
+            this.saveButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.saveButton.Location = new System.Drawing.Point(279, 427);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(138, 55);
+            this.saveButton.TabIndex = 6;
+            this.saveButton.Text = "save";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // resetButton
+            // 
+            this.resetButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resetButton.Location = new System.Drawing.Point(279, 488);
+            this.resetButton.Name = "resetButton";
+            this.resetButton.Size = new System.Drawing.Size(138, 55);
+            this.resetButton.TabIndex = 7;
+            this.resetButton.Text = "reset";
+            this.resetButton.UseVisualStyleBackColor = true;
+            this.resetButton.Click += new System.EventHandler(this.resetButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(681, 602);
+            this.Controls.Add(this.resetButton);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.stopButton);
+            this.Controls.Add(this.downButton);
+            this.Controls.Add(this.upButton);
+            this.Controls.Add(this.rightButton);
+            this.Controls.Add(this.noButton);
+            this.Controls.Add(this.leftButton);
+            this.Name = "Form1";
+            this.Text = "Solar Tracker GUI";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Button leftButton;
+        private System.Windows.Forms.Button noButton;
+        private System.Windows.Forms.Button rightButton;
+        private System.Windows.Forms.Button upButton;
+        private System.Windows.Forms.Button downButton;
+        private System.Windows.Forms.Button stopButton;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.Button resetButton;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Solar_Tracker_GUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            serialPort1.Open();
+        }
+
+        private void leftButton_Click(object sender, EventArgs e)
+        {
+            serialPort1.Write("l");
+        }
+
+        private void rightButton_Click(object sender, EventArgs e)
+        {
+            serialPort1.Write("r");
+        }
+
+        private void noButton_Click(object sender, EventArgs e)
+        {
+            serialPort1.Write("n");
+        }
+
+        private void upButton_Click(object sender, EventArgs e)
+        {
+            serialPort1.Write("u");
+        }
+
+        private void downButton_Click(object sender, EventArgs e)
+        {
+            serialPort1.Write("d");
+        }
+
+        private void stopButton_Click(object sender, EventArgs e)
+        {
+            serialPort1.Write("s");
+        }
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            serialPort1.Write("a");
+        }
+
+        private void resetButton_Click(object sender, EventArgs e)
+        {
+            serialPort1.Write("e");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
